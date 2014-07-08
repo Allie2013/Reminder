17392eb8fded8e39a87c516b9be23d6865d06b72
Cheenar Banerjee <cheenarb@fb.com>
2014-07-08 12:44:17 -0700

    Created new project

---
+++ RemindersV2/RemindersV2Tests/RemindersV2Tests.m
@@ -0,0 +1,34 @@
+//
+//  RemindersV2Tests.m
+//  RemindersV2Tests
+//
+//  Created by Cheenar Banerjee on 7/8/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RemindersV2Tests : XCTestCase
+
+@end
+
+@implementation RemindersV2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
